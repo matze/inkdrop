2355c6e13dcdca134e277283301fd7c1b1188bdf
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2021-03-27 14:58:33 +0100

    Move writing SVGs to lib

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use image::io::Reader;
-use image::GenericImageView;
 use inkdrop::point::Point;
 use inkdrop::tsp;
 use inkdrop::voronoi;
@@ -8,10 +7,6 @@ use log::info;
 use rayon::prelude::*;
 use std::path::PathBuf;
 use structopt::StructOpt;
-use svg::node::element::path::Data;
-use svg::node::element::Circle;
-use svg::node::element::Path;
-use svg::Document;
 
 #[derive(StructOpt)]
 pub struct Options {
@@ -40,51 +35,11 @@ pub struct Options {
     cmyk: bool,
 }
 
-fn draw_path(document: Document, tour: Vec<Point>, color: &str) -> Document {
-    if tour.is_empty() {
-        return document;
-    }
-
-    let mut data = Data::new().move_to((tour[0].x, tour[0].y));
-
-    for point in tour.into_iter().skip(1) {
-        data = data.line_to((point.x, point.y));
-    }
-
-    let path = Path::new()
-        .set("fill", "none")
-        .set("stroke", color)
-        .set("stroke-width", "1.0")
-        .set("d", data);
-
-    document.add(path)
-}
-
-fn draw_points(document: Document, points: Vec<Point>, color: &str) -> Document {
-    let mut document = document;
-
-    for point in points {
-        document = document.add(
-            Circle::new()
-                .set("fill", color)
-                .set("cx", point.x)
-                .set("cy", point.y)
-                .set("r", 1.0),
-        );
-    }
-
-    document
-}
-
 fn main() -> Result<()> {
     env_logger::init();
 
     let opt = Options::from_args();
     let img = Reader::open(&opt.input)?.decode()?;
-    let (width, height) = img.dimensions();
-    let colors = ["cyan", "magenta", "yellow", "black"];
-
-    let mut document = Document::new().set("viewBox", (0, 0, width, height));
 
     info!("Sample points");
     let mut point_sets = inkdrop::sample_points(&img, opt.num_points, opt.gamma, opt.cmyk);
@@ -101,11 +56,10 @@ fn main() -> Result<()> {
     }
 
     if opt.draw_points {
-        for (points, color) in point_sets.into_iter().zip(colors.iter()) {
-            document = draw_points(document, points, color);
-        }
+        inkdrop::write_points(&img, &opt.output, &point_sets)?;
     } else {
         info!("Make NN tours");
+
         let tours: Vec<Vec<Point>> = point_sets
             .into_par_iter()
             .map(|points| {
@@ -117,12 +71,8 @@ fn main() -> Result<()> {
             })
             .collect();
 
-        for (tour, color) in tours.into_iter().zip(colors.iter()) {
-            document = draw_path(document, tour, color);
-        }
+        inkdrop::write_path(&img, &opt.output, &tours)?;
     }
-
-    svg::save(&opt.output, &document)?;
 
     Ok(())
 }
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -3,8 +3,16 @@ pub mod point;
 pub mod tsp;
 pub mod voronoi;
 
+use anyhow::Result;
 use image::GenericImageView;
 use rand::Rng;
+use std::path;
+use svg::node::element::path::Data;
+use svg::node::element::Circle;
+use svg::node::element::Path;
+use svg::Document;
+
+const COLORS: [&str; 4] = ["cyan", "magenta", "yellow", "black"];
 
 pub fn sample_points(
     img: &image::DynamicImage,
@@ -42,4 +50,77 @@ pub fn sample_points(
     }
 
     ps
+}
+
+fn draw_path(document: Document, path: &Vec<point::Point>, color: &str) -> Document {
+    if path.is_empty() {
+        return document;
+    }
+
+    let mut data = Data::new().move_to((path[0].x, path[0].y));
+
+    for point in path.into_iter().skip(1) {
+        data = data.line_to((point.x, point.y));
+    }
+
+    let path = Path::new()
+        .set("fill", "none")
+        .set("stroke", color)
+        .set("stroke-width", "1.0")
+        .set("d", data);
+
+    document.add(path)
+}
+
+fn draw_points(document: Document, points: &Vec<point::Point>, color: &str) -> Document {
+    let mut document = document;
+
+    for point in points {
+        document = document.add(
+            Circle::new()
+                .set("fill", color)
+                .set("cx", point.x)
+                .set("cy", point.y)
+                .set("r", 1.0),
+        );
+    }
+
+    document
+}
+
+fn from_image(img: &image::DynamicImage) -> Document {
+    let (width, height) = img.dimensions();
+    Document::new().set("viewBox", (0, 0, width, height))
+}
+
+pub fn write_path(
+    img: &image::DynamicImage,
+    filename: &path::Path,
+    point_sets: &Vec<Vec<point::Point>>,
+) -> Result<()> {
+    let mut document = from_image(img);
+
+    for (path, color) in point_sets.into_iter().zip(COLORS.iter()) {
+        document = draw_path(document, path, color);
+    }
+
+    svg::save(filename, &document)?;
+
+    Ok(())
+}
+
+pub fn write_points(
+    img: &image::DynamicImage,
+    filename: &path::Path,
+    point_sets: &Vec<Vec<point::Point>>,
+) -> Result<()> {
+    let mut document = from_image(img);
+
+    for (points, color) in point_sets.into_iter().zip(COLORS.iter()) {
+        document = draw_points(document, points, color);
+    }
+
+    svg::save(filename, &document)?;
+
+    Ok(())
 }
